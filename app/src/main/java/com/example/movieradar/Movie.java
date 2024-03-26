cce36fdf9f544ad42c542625146c2819dfe4cd01
Jochem-tb <jochem.welp@gmail.com>
2024-03-26 11:45:54 +0100

    Create Movie.java + depend
    Creation of Movie.java and Genre.java following UML

---
+++ app/src/main/java/com/example/movieradar/Movie.java
@@ -0,0 +1,26 @@
+package com.example.movieradar;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Movie {
+    private int id;
+    private int imdb_id;
+    private Boolean adult;
+    private String backdrop_path;
+    private Long budget;
+    private ArrayList<Genre> genres;
+    private String original_language;
+    private String overview;
+    private Double popularity;
+    private String poster_path;
+    private LocalDate release_date;
+    private Long revenue;
+    private int runtime;
+    private String status;
+    private String title;
+    private Boolean video;
+    private float vote_average;
+    private int vote_count;
+
+}
