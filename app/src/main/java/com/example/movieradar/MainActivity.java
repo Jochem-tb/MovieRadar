2d48704d6d2990a198a2693aba4c574826f00a31
JanRoelofs <janroe2004@gmail.com>
2024-03-26 14:38:28 +0100

    Home Screen films en recyclerview layout toegevoegd

--- app/src/main/java/com/example/movieradar/MainActivity.java
+++ app/src/main/java/com/example/movieradar/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.movieradar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,36 +14,42 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private final String LOG_TAG = "MainActivity";
-    BottomNavigationView btmNavView;
+    private ArrayList<Movie> movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btmNavView = findViewById(R.id.btmNavViewMain);
+        RecyclerView rvMainMovieList = findViewById(R.id.rvMainMovieList);
+        rvMainMovieList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        Adapter = new MovieListAdapter(this, movieList);
+        rvMainMovieList.setAdapter(Adapter);
+
+        BottomNavigationView btmNavView = findViewById(R.id.btmNavViewMain);
         btmNavView.setSelectedItemId(R.id.menuHomeScreen);
 
         //NavBar functionality
         btmNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
                     int id = menuItem.getItemId();
                     //Heb geprobeerd met switch/case maar geeft errors
                     if (id == R.id.menuHomeScreen) {
-                        Intent HomeScreen= new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
                         Log.i(LOG_TAG, "HomeScreen button clicked");
                         return true;
                     } else if (id == R.id.menuFilmList) {
-                        Intent Catalogus = new Intent(MainActivity.this, Catalogus.class);
+                        startActivity(new Intent(MainActivity.this, Catalogus.class));
                         Log.i(LOG_TAG, "catalogus button clicked");
                         return true;
                     } else if (id == R.id.menuFilmList) {
-                        Intent Persoonlijk = new Intent(MainActivity.this, Persoonlijk.class);
+                        startActivity(new Intent(MainActivity.this, Persoonlijk.class));
                         Log.i(LOG_TAG, "Persoonlijk button clicked");
                         return true;
                     } else {
