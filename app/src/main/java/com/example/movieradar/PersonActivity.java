0a6ca352ce8eacec90eb76e7710b3f405168da87
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-03-27 14:49:08 +0100

    Renaming and updating of persoonlijk.java
    Renamed persoonlijk.java --> PersonActivity.java
    
    Added imageViews, textViews and buttons.

--- app/src/main/java/com/example/movieradar/PersonActivity.java
+++ app/src/main/java/com/example/movieradar/PersonActivity.java
@@ -3,25 +3,35 @@ package com.example.movieradar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class Persoonlijk extends AppCompatActivity {
-    private final String LOG_TAG = "Persoonlijk";
+public class PersonActivity extends AppCompatActivity {
+    private final String LOG_TAG = "PersonActivity";
     BottomNavigationView btmNavView;
-
+    ImageView ivAccountImage;
+    TextView tvNameAccount;
+    Button bMore;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.persoonlijk);
+        setContentView(R.layout.activity_person);
 
         btmNavView = findViewById(R.id.btmNavViewMain);
         btmNavView.setSelectedItemId(R.id.menuHomeScreen);
+
+        ivAccountImage = findViewById(R.id.ivAccountImage);
+        tvNameAccount = findViewById(R.id.tvNamePerson);
+        bMore = findViewById(R.id.bMore);
 
         //NavBar functionality
         btmNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -31,16 +41,16 @@ public class Persoonlijk extends AppCompatActivity {
                 int id = menuItem.getItemId();
                 //Heb geprobeerd met switch/case maar geeft errors
                 if (id == R.id.menuHomeScreen) {
-                    Intent HomeScreen= new Intent(MainActivity.this, MainActivity.class);
+//                    Intent HomeScreen= new Intent(MainActivity.this, MainActivity.class);
                     Log.i(LOG_TAG, "HomeScreen button clicked");
                     return true;
                 } else if (id == R.id.menuFilmList) {
-                    Intent Catalogus = new Intent(MainActivity.this, Catalogus.class);
+//                    Intent Catalogus = new Intent(MainActivity.this, Catalogus.class);
                     Log.i(LOG_TAG, "catalogus button clicked");
                     return true;
-                } else if (id == R.id.menuFilmList) {
-                    Intent Persoonlijk = new Intent(MainActivity.this, Persoonlijk.class);
-                    Log.i(LOG_TAG, "Persoonlijk button clicked");
+                } else if (id == R.id.menuPersonal) {
+//                    Intent Persoonlijk = new Intent(MainActivity.this, PersonActivity.class);
+                    Log.i(LOG_TAG, "Person button clicked");
                     return true;
                 } else {
                     return false;
