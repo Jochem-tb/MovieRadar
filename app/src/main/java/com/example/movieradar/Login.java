bb045cfdbf01bd3b47acc671b60560023c342e5f
TheLobbyPigeon <gijsvanvugt2002@gmail.com>
2024-03-28 14:27:16 +0100

    Functionaliteit toegevoegd aan login pagina

--- app/src/main/java/com/example/movieradar/Login.java
+++ app/src/main/java/com/example/movieradar/Login.java
@@ -1,24 +1,105 @@
 package com.example.movieradar;
 
+import static com.example.movieradar.R.id.BirthdateButton;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.movieradar.R.id;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Login {
+public class Login extends AppCompatActivity {
 
     private TextView EmailUserField;
-
     private TextView PasswordField;
+    private TextView BirthdateView;
+    private ImageView BirthDateButton;
+    private TextView EmailField;
+    private TextView UserField;
+    private TextView PassField;
+    private Button InlogButton;
+    private Button RegisterButton;
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginactivity);
 
-    boolean CheckLoginAuthentication(TextView EmailUserField, TextView PasswordField){
+        // on below line we are initializing our variables.
+        BirthDateButton = findViewById(R.id.BirthdateButton);
+        BirthdateView = findViewById(R.id.BirthdateView);
+
+        // on below line we are adding click listener for our pick date button
+        BirthDateButton.setOnClickListener(v -> {
+            // on below line we are getting
+            // the instance of our calendar.
+            final Calendar c = Calendar.getInstance();
+
+            // on below line we are getting
+            // our day, month and year.
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // on below line we are creating a variable for date picker dialog.
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    // on below line we are passing context.
+                    Login.this,
+                    (view, year1, monthOfYear, dayOfMonth) -> {
+                        // on below line we are setting date to our text view.
+                        BirthdateView.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year1);
+
+                    },
+                    // on below line we are passing year,
+                    // month and day for selected date in our date picker.
+                    year, month, day);
+            // at last we are calling show to
+            // display our date picker dialog.
+            datePickerDialog.show();
+        });
+
+        EmailField = findViewById(id.EmailField);
+        UserField = findViewById(id.UserField);
+        PassField = findViewById(id.PassField);
+        InlogButton = findViewById(id.InlogButton);
+        RegisterButton = findViewById(id.RegisterButton);
+        EmailUserField = findViewById(id.EmailUserField);
+        PasswordField = findViewById(id.PasswordField);
+        InlogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckLoginAuthentication((String) EmailUserField.getText(), (String) PasswordField.getText());
+            }
+        });
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    DatabaseCon.sendCommand(DatabaseCon.createConnection(),"INSERT INTO MR-Gebruiker(EmailAdres,Gebruikersnaam,Wachtwoord,Geboortedatum) VALUES (" +
+                            EmailField.getText() + "," +
+                            UserField.getText() + "," +
+                            PassField.getText() + "," +
+                            BirthdateView.getText() + ")");
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+    }
+    boolean CheckLoginAuthentication (String User, String Password){
         try {
-            ResultSet rs = DatabaseCon.createConnection().prepareStatement("SELECT * FROM MR-Gebruiker WHERE UserName='" + EmailUserField + "' AND PassWord='" + PasswordField + "'").executeQuery();
+            ResultSet rs = DatabaseCon.createConnection().prepareStatement("SELECT * FROM MR-Gebruiker WHERE Gebruikersnaam='" + User + "' AND Wachtwoord='" + Password + "'").executeQuery();
 
             return rs.next();
         } catch (SQLException ex) {
@@ -26,6 +107,5 @@ public class Login {
         }
         return false;
     }
-
-    }
+}
 
--- app/src/main/java/com/example/movieradar/PersonActivity.java
+++ app/src/main/java/com/example/movieradar/PersonActivity.java
@@ -1,22 +1,15 @@
 package com.example.movieradar;
 
-import static com.example.movieradar.R.id.idBtnPickDate;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
-import java.util.Calendar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -29,47 +22,11 @@ public class PersonActivity extends AppCompatActivity {
     TextView tvNameAccount;
     Button bMore;
 
-    private TextView selectedDateTV;
-
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
-
-
-        // on below line we are initializing our variables.
-        ImageView pickDateBtn = findViewById(idBtnPickDate);
-        selectedDateTV = findViewById(R.id.idTVSelectedDate);
-
-        // on below line we are adding click listener for our pick date button
-        pickDateBtn.setOnClickListener(v -> {
-            // on below line we are getting
-            // the instance of our calendar.
-            final Calendar c = Calendar.getInstance();
-
-            // on below line we are getting
-            // our day, month and year.
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // on below line we are creating a variable for date picker dialog.
-            DatePickerDialog datePickerDialog = new DatePickerDialog(
-                    // on below line we are passing context.
-                    Persoonlijk.this,
-                    (view, year1, monthOfYear, dayOfMonth) -> {
-                        // on below line we are setting date to our text view.
-                        selectedDateTV.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year1);
-
-                    },
-                    // on below line we are passing year,
-                    // month and day for selected date in our date picker.
-                    year, month, day);
-            // at last we are calling show to
-            // display our date picker dialog.
-            datePickerDialog.show();
-        });
 
 //        btmNavView = findViewById(R.id.btmNavViewMain);
 //        btmNavView.setSelectedItemId(R.id.menuHomeScreen);
