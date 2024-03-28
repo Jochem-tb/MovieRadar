45e650702fbd5bf2f6972ac77fdf21afc18ed50f
Jochem-tb <jochem.welp@gmail.com>
2024-03-28 11:42:00 +0100

    Added AlertDialog List of Lists
    Een Alertdialog aangemaakt voor het selecteren van de lijsten waar de film in toegevoegd is / word.
    
    Bijbehorende logica beter verspreid, in methodes.
    Long click listerner opgesteld voor het tonen van de Dialog.
    OK en CANCEL Button functionerend.

--- app/src/main/java/com/example/movieradar/MovieDetailActivity.java
+++ app/src/main/java/com/example/movieradar/MovieDetailActivity.java
@@ -1,16 +1,21 @@
 package com.example.movieradar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,8 +37,11 @@ public class MovieDetailActivity extends AppCompatActivity {
     ImageView mMovieBackground;
 
     CircleImageView mFavoriteButton;
-    CircleImageView  mTrailerButton;
+    CircleImageView mTrailerButton;
     Button mBuyTicketButton;
+
+    LinearLayout checkboxContainter;
+    AlertDialog listDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +74,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         mMovie = (Movie) intent.getSerializableExtra(Movie.getKey());
 
         //Movie data invullen
-        if(mMovie != null){
+        if (mMovie != null) {
             toolbar.setTitle(mMovie.getTitle());
             mMovieTime.setText(String.valueOf(mMovie.getRuntime()));
             mMovieRating.setText(String.valueOf(mMovie.getVote_average()));
@@ -74,18 +82,76 @@ public class MovieDetailActivity extends AppCompatActivity {
             Picasso.get().load(APIString.getBackdropUrl(mMovie.getBackdrop_path())).into(mMovieBackground);
         }
 
+
+        listDialog = setupListDialogView();
+        setupLongButtonListener(listDialog);
+        setupNormalButtonListerners();
+
+
+    }
+
+    private void setupLongButtonListener(AlertDialog listDialog) {
+        Log.d(LOG_TAG, "setupLongButtonListener");
+        mFavoriteButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                listDialog.show();
+                return false;
+            }
+        });
+    }
+
+    private AlertDialog setupListDialogView() {
+        Log.d(LOG_TAG, "setupListDialogView");
+        //Infalting de layout.xml
+        View listDialog = LayoutInflater.from(this).inflate(R.layout.list_adding_dialog_layout, null);
+
+        //checkboxContainter koppelen aan id
+        checkboxContainter = listDialog.findViewById(R.id.checkbox_container);
+
+        //Ophalen van hoeveelheid lijsten, waar gebruiker toegang tot heeft.
+//        getNumLists();
+        int numCheckBox = 10;
+
+        //Plaats numCheckBox CheckBoxen in de Containter
+        for (int i = 0; i < numCheckBox; i++) {
+            CheckBox checkBox = new CheckBox(this);
+            //TODO Methode voor ophalen lijst namen
+            checkBox.setText("LIJST NAAM " + (i + 1));
+            checkboxContainter.addView(checkBox);
+        }
+
+        //Aanmaken & tonen va Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(listDialog)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getBaseContext(), String.valueOf(which), Toast.LENGTH_SHORT).show();
+                        //TODO apply changes op lijsten & database
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        //Retourneer opgebouwde Dialog
+        AlertDialog l = builder.create();
+        return l;
+    }
+
+
+
+    private void setupNormalButtonListerners() {
+        Log.d(LOG_TAG, "setupNormalButtonListerners");
         //Button listerners
         mFavoriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(v.getContext(),"Favorite Button Kort", Toast.LENGTH_SHORT).show();
-            }
-        });
-        mFavoriteButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(v.getContext(),"Favorite Button Lang", Toast.LENGTH_SHORT).show();
-                return false;
             }
         });
         mTrailerButton.setOnClickListener(new View.OnClickListener() {
