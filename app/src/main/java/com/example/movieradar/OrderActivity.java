c1bfaf916a8f1c647a3e49e7cd406ae1405ce969
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-04-02 20:35:41 +0200

    Creation of OrderActivity class
    + LOG TAG = "OrderActivity"

---
+++ app/src/main/java/com/example/movieradar/OrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.movieradar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "OrderActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
